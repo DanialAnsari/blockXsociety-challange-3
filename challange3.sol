fc1e19cb8522abf8944361dc742dfe1ca68b5463
Danial Ansari <danialansari998@gmail.com>
2020-11-13 22:10:30 +0500

    Add files via upload

---
+++ challange3.sol
@@ -0,0 +1,92 @@
+pragma solidity ^0.6.0;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address payable) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
+
+contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () internal {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+
+
+
+contract Challange3 is Ownable{
+    
+   
+    enum taskStatus{
+        DOESNOTEXIT,
+        Incomplete,
+        Completed
+    }
+    
+    mapping(string => taskStatus) public toDoList;
+    
+    function AddTasks(string memory task) public  onlyOwner{
+        
+      toDoList[task] = taskStatus.Incomplete;
+    }
+    
+    function taskCompleted(string memory task) public onlyOwner{
+        require(toDoList[task]==taskStatus.Incomplete,"Task does not Exist or is already Completed");
+        toDoList[task] = taskStatus.Completed;
+    }
+    
+}
+
